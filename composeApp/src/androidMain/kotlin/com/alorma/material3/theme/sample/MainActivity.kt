90bfd3e72469763f27d5652e0936cea106f01118
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-04 19:52:21 +0100

    Add some themming

--- composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
@@ -13,14 +13,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
   @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      MaterialTheme {
+      MaterialTheme(
+        colorScheme = dynamicLightColorScheme(LocalContext.current)
+      ) {
         Scaffold(
           topBar = { TopAppBar(title = { Text(text = "Desktop demp") }) },
         ) { padding ->
