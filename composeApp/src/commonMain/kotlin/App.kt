45b4d6792b626016cd7b9838410b3b876e8a74b6
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-04 09:43:56 +0100

    Add toolbar

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,9 +1,21 @@
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
 fun App() {
   MaterialTheme {
-    LibComponent()
+    Scaffold(
+      topBar = {
+        TopAppBar(
+          backgroundColor = MaterialTheme.colors.background,
+          title = { Text(text = getLibraryPlatformName()) },
+        )
+      },
+    ) {
+      LibComponent()
+    }
   }
 }
