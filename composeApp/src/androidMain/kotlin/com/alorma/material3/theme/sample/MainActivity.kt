7981736d134c45114ffd0786b241cfd711885d47
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-05 10:25:09 +0100

    Check for dynamic Colors

--- composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.alorma.material3.theme.sample
 
 import ThemeViewer
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 
@@ -22,8 +24,13 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
+      val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        dynamicLightColorScheme(LocalContext.current)
+      } else {
+        lightColorScheme()
+      }
       MaterialTheme(
-        colorScheme = dynamicLightColorScheme(LocalContext.current)
+        colorScheme = colorScheme,
       ) {
         Scaffold(
           topBar = { TopAppBar(title = { Text(text = "Desktop demp") }) },
