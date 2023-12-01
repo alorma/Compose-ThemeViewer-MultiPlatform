16c1a2e0c200636744e3874a8736a90283f8759b
Bernat Borrás <bernat.borras@adevinta.com>
2023-12-01 20:02:20 +0100

    Custom colors on the Theme Viewer

--- composeApp/src/commonMain/kotlin/com.alorma.material3.theme.sample/App.kt
+++ composeApp/src/commonMain/kotlin/com.alorma.material3.theme.sample/App.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.alorma.material3.theme.ThemeViewer
+import com.alorma.material3.theme.sample.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
@@ -18,13 +19,39 @@ fun App() {
   Scaffold(
     topBar = { TopAppBar(title = { Text(text = "Theme Viewer (${Platform.name})") }) },
   ) { padding ->
+    val colorScheme = AppTheme.colorScheme
     ThemeViewer(
       modifier = Modifier
         .consumeWindowInsets(padding)
         .padding(top = padding.calculateTopPadding())
         .fillMaxSize(),
     ) {
-
+      header { Text(text = "Success color") }
+      color(
+        containerColor = colorScheme.success,
+        contentColor = colorScheme.onSuccess,
+        containerText = "success",
+      )
+      subHeader { Text(text = "onSuccess") }
+      color(
+        containerColor = colorScheme.onSuccess,
+        contentColor = colorScheme.success,
+        containerText = "onSuccess",
+        showBorder = true,
+      )
+      header { Text(text = "Alert color") }
+      color(
+        containerColor = colorScheme.alert,
+        contentColor = colorScheme.onAlert,
+        containerText = "alert",
+      )
+      subHeader { Text(text = "onAlert") }
+      color(
+        containerColor = colorScheme.onAlert,
+        contentColor = colorScheme.alert,
+        containerText = "onAlert",
+        showBorder = true,
+      )
     }
   }
 }
