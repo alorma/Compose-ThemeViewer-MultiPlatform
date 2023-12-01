80b40893a9da1b4cbf9cc74e8405c1d6a1037a7e
Bernat Borrás <bernat.borras@adevinta.com>
2023-12-01 17:25:32 +0100

    Show some prinary colors

--- material3-theme-viewer/src/commonMain/kotlin/ThemeViewer.kt
+++ material3-theme-viewer/src/commonMain/kotlin/ThemeViewer.kt
@@ -1,9 +1,9 @@
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,14 +12,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -28,6 +29,7 @@ fun ThemeViewer(
   modifier: Modifier = Modifier,
   customContent: LazyListScope.() -> Unit = {},
 ) {
+  val isDark = isSystemInDarkTheme()
   val colorScheme = MaterialTheme.colorScheme
 
   LazyColumn(
@@ -38,22 +40,23 @@ fun ThemeViewer(
     headerItem("Primary colors")
     colorItem(
       containerColor = colorScheme.primary,
-      contentColor = colorScheme.onPrimary,
       containerText = "primary",
-      contentText = "onPrimary",
     )
     colorItem(
       containerColor = colorScheme.primaryContainer,
-      contentColor = colorScheme.onPrimaryContainer,
       containerText = "primaryContainer",
-      contentText = "onPrimaryContainer",
     )
-    headerItem("Error colors")
+    subHeaderItem("onPrimary")
     colorItem(
-      containerColor = colorScheme.errorContainer,
-      contentColor = colorScheme.onErrorContainer,
-      containerText = "errorContainer",
-      contentText = "onErrorContainer",
+      containerColor = colorScheme.onPrimary,
+      contentColor = colorScheme.primary,
+      borderColor = !isDark,
+      containerText = "onPrimary",
+    )
+    colorItem(
+      containerColor = colorScheme.onPrimaryContainer,
+      contentColor = colorScheme.primaryContainer,
+      containerText = "onPrimaryContainer",
     )
     customContent()
   }
@@ -62,7 +65,9 @@ fun ThemeViewer(
 @OptIn(ExperimentalFoundationApi::class)
 private fun LazyListScope.headerItem(text: String) {
   stickyHeader {
-    Surface {
+    Surface(
+      modifier = Modifier.height(48.dp).fillMaxWidth(),
+    ) {
       Box(
         contentAlignment = Alignment.CenterStart
       ) {
@@ -75,28 +80,51 @@ private fun LazyListScope.headerItem(text: String) {
   }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.subHeaderItem(text: String) {
+  stickyHeader {
+    Surface(
+      modifier = Modifier.height(48.dp).fillMaxWidth(),
+    ) {
+      Box(
+        contentAlignment = Alignment.CenterStart
+      ) {
+        Text(
+          text = text,
+          style = MaterialTheme.typography.titleSmall,
+        )
+      }
+    }
+  }
+}
+
 private fun LazyListScope.colorItem(
   containerColor: Color,
-  contentColor: Color,
+  contentColor: Color = Color.Unspecified,
+  borderColor: Boolean = false,
   containerText: String,
-  contentText: String
 ) {
   item {
     colorCard(
       containerColor = containerColor,
-      contentColor = contentColor,
+      contentColor = if (contentColor == Color.Unspecified) {
+        contentColorFor(containerColor)
+      } else {
+        contentColor
+      },
       containerText = containerText,
-      contentText = contentText,
+      showBorder = borderColor,
     )
   }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun colorCard(
   containerColor: Color,
-  contentColor: Color,
+  contentColor: Color = contentColorFor(containerColor),
   containerText: String,
-  contentText: String
+  showBorder: Boolean,
 ) {
   Card(
     modifier = Modifier.height(80.dp).fillMaxWidth(),
@@ -104,22 +132,22 @@ private fun colorCard(
       containerColor = containerColor,
       contentColor = contentColor,
     ),
+    onClick = {},
+    border = if (showBorder) {
+      CardDefaults.outlinedCardBorder()
+    } else {
+      null
+    }
   ) {
     Column(
       modifier = Modifier.fillMaxSize().padding(8.dp),
       verticalArrangement = Arrangement.Bottom,
-      horizontalAlignment = Alignment.Start,
+      horizontalAlignment = Alignment.End,
     ) {
       Text(
         text = containerText,
         style = MaterialTheme.typography.bodyMedium,
         fontWeight = FontWeight.Bold,
-      )
-      Spacer(modifier = Modifier.height(4.dp))
-      Text(
-        text = contentText,
-        style = MaterialTheme.typography.labelSmall,
-        fontStyle = FontStyle.Italic,
       )
     }
   }
