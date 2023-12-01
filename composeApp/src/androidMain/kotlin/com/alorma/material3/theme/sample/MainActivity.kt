e45335daacf09251a614e32536425a601d358bf1
Bernat Borrás <bernat.borras@adevinta.com>
2023-12-01 16:34:06 +0100

    Improve demo

--- composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/alorma/material3/theme/sample/MainActivity.kt
@@ -1,26 +1,15 @@
 package com.alorma.material3.theme.sample
 
-import ThemeViewer
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
-  @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
@@ -29,20 +18,7 @@ class MainActivity : ComponentActivity() {
       } else {
         lightColorScheme()
       }
-      MaterialTheme(
-        colorScheme = colorScheme,
-      ) {
-        Scaffold(
-          topBar = { TopAppBar(title = { Text(text = "Desktop demp") }) },
-        ) { padding ->
-          ThemeViewer(
-            modifier = Modifier
-              .consumeWindowInsets(padding)
-              .padding(top = padding.calculateTopPadding())
-              .fillMaxSize(),
-          )
-        }
-      }
+      App(colorScheme = colorScheme)
     }
   }
 }
